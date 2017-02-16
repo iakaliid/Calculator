f26280df208f605421a7c0c3edb03d0677b18dc4
Lewis D <lewis.dackam@gmail.com>
2017-02-15 23:28:13 -0500

    implemented calculator - WIP

--- calculator.js
+++ calculator.js
@@ -1,1 +1,8 @@
-function (a, b) { a + b }
+const calculator = (a, b) => { 
+  return a + b;
+};
+
+//my implementation is working
+const result = calculator(10,10); //20
+
+console.log('the result of the calculation is', result);
